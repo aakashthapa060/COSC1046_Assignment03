d14bc9fcd0267d135f74304dffdf1d5ed2edd197
Aakash Thapa <aakashmanm3@gmail.com>
2023-10-13 21:36:43 -0400

    Assignment Init

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Sales Amount: ");
+        double amount = input.nextDouble();
+        double validatedAmount = validateInput(amount);
+        double salesCommission = calculateSalesCommission(validatedAmount);
+        System.out.println("The commission is $" + salesCommission + " for sales amount of $" + validatedAmount);
+
+    }
+
+    public static double validateInput(double num) {
+        Scanner input = new Scanner(System.in);
+        while (num < 1) {
+            System.out.println("Please Enter a Valid Sales Amount: ");
+            num = input.nextDouble();
+        }
+        return num;
+    }
+
+    public static double calculateSalesCommission(double salesAmount) {
+        int commissionRate;
+
+        if (salesAmount < 5000) {
+            commissionRate = 8;
+        } else if (salesAmount >= 5001 && salesAmount <= 10000) {
+            commissionRate = 10;
+        } else {
+            commissionRate = 12;
+
+        }
+
+        double salesCommission = (salesAmount / 100) * commissionRate;
+
+        return salesCommission;
+    }
+
+}
