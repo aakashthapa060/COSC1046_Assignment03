460f9e09e6f00ac8a73fd584d3126a3c97b60970
Aakash Thapa <aakashmanm3@gmail.com>
2023-10-16 13:29:15 -0400

    Sales Amount Finished

--- SalesAmount.java
+++ SalesAmount.java
@@ -2,6 +2,40 @@ import java.util.Scanner;
 
 public class SalesAmount {
     public static void main(String[] args) {
+        double validatedAmount = validateInput();
+        // Display the calculated sales Amount
+        double salesAmount = calculateSalesAmount(validatedAmount);
+        System.out.println("The Sales Amount is $" + salesAmount + " for sales commission of $" + validatedAmount);
+    }
 
+    public static double validateInput() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Sales Amount: ");
+        double amount = input.nextDouble();
+        while (amount < 1) {
+            System.out.println("Please Enter a Valid Sales Amount: ");
+            amount = input.nextDouble();
+        }
+        return amount;
+    }
+
+    public static double calculateSalesAmount(double salesCommission) {
+        int commissionRate;
+
+        if (salesCommission <= 400) {
+            commissionRate = 8;
+        } else if (salesCommission <= 1000) {
+            commissionRate = 10;
+
+        } else {
+            commissionRate = 12;
+        }
+
+        double salesAmount = (salesCommission / commissionRate) * 100;
+        return salesAmount;
     }
 }
+
+// 400
+// 1000
--- SalesCommission.java
+++ SalesCommission.java
@@ -2,13 +2,8 @@ import java.util.Scanner;
 
 public class SalesCommission {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter Sales Amount: ");
-        double amount = input.nextDouble();
-
-        double validatedAmount = validateInput(amount);
-
+        double validatedAmount = validateInput();
         // Display the calculated sales Commission
         double salesCommission = calculateSalesCommission(validatedAmount);
         System.out.println("The commission is $" + salesCommission + " for sales amount of $" + validatedAmount);
@@ -18,13 +13,16 @@ public class SalesCommission {
     /*
      * Function to Validate given amount
      */
-    public static double validateInput(double num) {
+    public static double validateInput() {
         Scanner input = new Scanner(System.in);
-        while (num < 1) {
+
+        System.out.println("Enter Sales Amount: ");
+        double amount = input.nextDouble();
+        while (amount < 1) {
             System.out.println("Please Enter a Valid Sales Amount: ");
-            num = input.nextDouble();
+            amount = input.nextDouble();
         }
-        return num;
+        return amount;
     }
 
     // Function to calculate sales amount based on commission
